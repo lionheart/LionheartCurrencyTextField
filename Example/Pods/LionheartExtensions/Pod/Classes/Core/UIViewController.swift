bf2338936bb0d7e86e963b295d809e669e4c11ea
Dan Loewenherz <dan@lionheartsw.com>
2016-09-09 14:51:18 -0500

    update pod build files

---
+++ Example/Pods/LionheartExtensions/Pod/Classes/Core/UIViewController.swift
@@ -0,0 +1,63 @@
+//
+//  Copyright 2016 Lionheart Software LLC
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+//
+
+import Foundation
+
+public extension UIViewController {
+    /**
+     Returns an optional with the top-level `UIViewController`.
+     
+     - returns: The active `UIViewController`, if it can be found.
+     - author: Daniel Loewenherz
+     - copyright: ©2016 Lionheart Software LLC
+     - date: February 17, 2016
+     */
+    func topViewController() -> UIViewController? {
+        return UIViewController.topViewControllerWithRootViewController(UIApplication.sharedApplication().keyWindow?.rootViewController)
+    }
+
+    /**
+     Return an optional with the top-level `UIViewController` for the provided `UIViewController`.
+     
+     - returns: The active `UIViewController`, if it can be found.
+     - author: Daniel Loewenherz
+     - copyright: ©2016 Lionheart Software LLC
+     - date: February 17, 2016
+     */
+    class func topViewControllerWithRootViewController(rootViewController: UIViewController?) -> UIViewController? {
+        if let tabBarController = rootViewController as? UITabBarController {
+            return UIViewController.topViewControllerWithRootViewController(tabBarController.selectedViewController)
+        }
+        else if let navigationController = rootViewController as? UINavigationController {
+            return UIViewController.topViewControllerWithRootViewController(navigationController.visibleViewController)
+        }
+        else if let splitViewController = rootViewController as? UISplitViewController {
+            return UIViewController.topViewControllerWithRootViewController(splitViewController.viewControllers[1])
+        }
+        else if let presentedViewController = rootViewController?.presentedViewController {
+            if presentedViewController is UIAlertController {
+                return presentedViewController
+            }
+            else {
+                return UIViewController.topViewControllerWithRootViewController(presentedViewController)
+            }
+        }
+        else {
+            return rootViewController
+        }
+    }
+}
---
+++ Example/Pods/LionheartExtensions/Pod/Classes/File.swift
@@ -0,0 +1,94 @@
+//
+//  Copyright 2016 Lionheart Software LLC
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import Foundation
+
+final class File: StringLiteralConvertible {
+    var filename: String?
+
+    lazy var documentsPath: String? = {
+        let paths: [NSString] = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
+        if let path = paths.first,
+            filename = self.filename {
+                return path.stringByAppendingPathComponent(filename)
+        }
+        else {
+            return nil
+        }
+    }()
+
+    var bundlePath: String? {
+        let bundle = NSBundle.mainBundle()
+        if let filename = filename {
+            let components = filename.componentsSeparatedByString(".")
+            return bundle.pathForResource(components[0], ofType: components[1])
+        }
+        else {
+            return nil
+        }
+    }
+
+    typealias ExtendedGraphemeClusterLiteralType = StringLiteralType
+    typealias UnicodeScalarLiteralType = StringLiteralType
+
+    init(extendedGraphemeClusterLiteral value: ExtendedGraphemeClusterLiteralType) {
+        filename = value
+    }
+
+    init(stringLiteral value: StringLiteralType) {
+        filename = value
+    }
+
+    init(unicodeScalarLiteral value: UnicodeScalarLiteralType) {
+        filename = value
+    }
+
+    func read() -> String? {
+        var contents: String?
+        let path = documentsPath ?? bundlePath
+
+        if let path = path {
+            do {
+                contents = try String(contentsOfFile: path, encoding: NSUTF8StringEncoding)
+            }
+            catch {
+                // MARK: TODO
+            }
+        }
+
+        return contents
+    }
+
+    func existsInBundle() -> Bool {
+        let manager = NSFileManager.defaultManager()
+
+        guard let path = bundlePath else {
+            return false
+        }
+
+        return manager.fileExistsAtPath(path)
+    }
+
+    func existsInDocuments() -> Bool {
+        let manager = NSFileManager.defaultManager()
+
+        guard let path = documentsPath else {
+            return false
+        }
+
+        return manager.fileExistsAtPath(path)
+    }
+}
